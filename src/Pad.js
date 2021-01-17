caa58124de5d5005a3f5f63f251d90e20e7bf5d6
Tejas <tejasmishra2k18@gmail.com>
2021-01-18 01:00:03 +0530

    create and align buttons and fix relative sizing

--- src/Pad.js
+++ src/Pad.js
@@ -4,7 +4,20 @@ class Pad extends React.Component {
     render() {
         return (
             <div id="pad">
-                <div id="mid">                   
+                <div className="padButton" id="menu" style={{fontWeight:'bolder'}}>
+                    <i class="fas fa-bars"></i>
+                </div> 
+                <div id="midRow">
+                    <div className="padButton" id="backward" style={{marginRight:12}}>
+                        <i class="fas fa-fast-backward"></i>
+                    </div>
+                    <div id="mid"></div>
+                    <div className="padButton" id="forward" style={{marginLeft:12}}>
+                        <i class="fas fa-fast-forward"></i>
+                    </div>
+                </div>
+                <div className="padButton" id="playpause">
+                    <i class="fas fa-play"></i><i class="fas fa-pause"></i>
                 </div>
             </div>
         )
