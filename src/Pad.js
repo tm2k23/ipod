095d7ab92fc91c66dcc03a7ea4ceed337505d36c
Tejas <tejasmishra2k18@gmail.com>
2021-01-18 13:44:54 +0530

    setup zingtouch-error

--- src/Pad.js
+++ src/Pad.js
@@ -1,18 +1,19 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import ZingTouch from 'zingtouch';
 class Pad extends React.Component {
     render() {
         return (
             <div id="pad">
-                <div className="padButton" id="menu" style={{fontWeight:'bolder'}}>
+                <div className="padButton" id="menu" style={{ fontWeight: 'bolder' }}>
                     <i class="fas fa-bars"></i>
-                </div> 
+                </div>
                 <div id="midRow">
-                    <div className="padButton" id="backward" style={{marginRight:12}}>
+                    <div className="padButton" id="backward" style={{ marginRight: 12 }}>
                         <i class="fas fa-fast-backward"></i>
                     </div>
                     <div id="mid"></div>
-                    <div className="padButton" id="forward" style={{marginLeft:12}}>
+                    <div className="padButton" id="forward" style={{ marginLeft: 12 }}>
                         <i class="fas fa-fast-forward"></i>
                     </div>
                 </div>
@@ -21,7 +22,14 @@ class Pad extends React.Component {
                 </div>
             </div>
         )
-    };   
+    };
 }
+
+// var touchArea = document.getElementById("pad");
+// var myRegion = new ZingTouch.Region(touchArea);
+
+// myRegion.bind(touchArea, 'rotate', function(event) {
+//     console.log(event);
+// });
 
 export default Pad;
