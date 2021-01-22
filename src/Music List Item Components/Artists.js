d59d09a6522ce96e5ae468f31253668035dcd3c0
Tejas <tejasmishra2k18@gmail.com>
2021-01-22 23:53:07 +0530

    .

--- src/Music List Item Components/Artists.js
+++ src/Music List Item Components/Artists.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import '../stylesheets/artist.css';
+import '../stylesheets/artist.css'; // import the stylesheet of this component 
 const Artists = (props) => {
     return(
         <div className="screenContentDiv">
             <div className="screenHeader">
+                {/* this is the screen header as in all the components  */}
                 <span className="ipod-icon">iPod</span>
                 <span className="battery"><i class="fas fa-battery-three-quarters"></i></span>
             </div>
             <div id="artistDiv">
+                {/* this is the artist div */}
                 <img src="logo512.png"></img>
                 <br></br>
                 <div>React Mini-Project</div>
@@ -18,4 +19,4 @@ const Artists = (props) => {
         </div>
     )
 }
-export default Artists;
+export default Artists; // export the component 
